4c8fa623530f8f71e40571b57c15211452eb1e8f
Varvi <warwarasaworska@gmail.com>
2026-02-07 16:10:45 +0100

    chb juz

--- menedzer zadan/dane.py
+++ menedzer zadan/dane.py
@@ -1,7 +1,3 @@
 tasks = [
-{
-"title ": "Nauczyc sie Pythona ",
-"done ": False ,
-"due_date ": " 2026 -01 -20 "
-}
+    
 ]
--- menedzer zadan/funkcje.py
+++ menedzer zadan/funkcje.py
@@ -1,14 +1,7 @@
-#from dane import tasks
+from dane import tasks
 from datetime import date
 import json
 import os
-tasks = [
-{
-"title ": "Nauczyc sie Pythona ",
-"done ": False ,
-"due_date ": " 2026 -01 -20 "
-}
-]
 
 
 def add_new_task(title: str, due_date: date | None = None):
@@ -33,15 +26,19 @@ def load_tasks():
         tasks = []
 
 def show_tasks(which_tasks):
-	print("\ntwoje zadania:\n")
 	if which_tasks == 0:
-		print(tasks)
+		for task in tasks:
+			print(task, "nr zadania: ", tasks.index(task))
+			print()
 	elif which_tasks == 1 :
 		for task in tasks:
 			if task["done "] == False:
-				print(task)
+				print(task, "nr zadania: ", tasks.index(task))
+				print()
 	elif which_tasks == 2:
 		for task in tasks:
 			if task["done "] == True:
-				print(task)
+				print(task,"nr zadania: ", tasks.index(task))
+				print()
+
 		
